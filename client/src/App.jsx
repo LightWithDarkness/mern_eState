3aa6ce3cf5759b2564cd8775a3813b028e4ed50a
LightWithDarkness <mannukumarverma13@gmail.com>
2024-08-12 02:44:18 +0530

    added pages with routes and Header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,24 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+
 const App = () => {
+
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/about' element={<About />}/>
+      <Route path='/sign-in' element={<SignIn />}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/Profile' element={<Profile />}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 export default App
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,44 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3 sm:px-4">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Kumar</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
+        <form
+          action=""
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
+          <input
+            type="text"
+            placeholder="search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch />
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to='sign-in'>
+            <li className="text-slate-700 hover:underline">Sign In</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+};
+export default Header;
